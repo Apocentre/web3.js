c556d76e37a825205f2e200d0b012a9006efb144
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2021-06-24 14:04:04 +0300

    log error response

--- packages/web3-eth-abi/src/index.js
+++ packages/web3-eth-abi/src/index.js
@@ -348,6 +348,7 @@ ABICoder.prototype.decodeParameters = function (outputs, bytes) {
  */
 ABICoder.prototype.decodeParametersWith = function (outputs, bytes, loose) {
     if (outputs.length > 0 && (!bytes || bytes === '0x' || bytes === '0X')) {
+        console.error({outputs, bytes, loose})
         throw new Error(
             'Returned values aren\'t valid, did it run Out of Gas? ' +
             'You might also see this error if you are not using the ' +
