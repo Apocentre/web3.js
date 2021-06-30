871dcdfc86a72f0a70adf25aca8a3172c72f0bfe
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2021-06-30 10:55:06 +0300

    Include log in error message

--- packages/web3-eth-abi/src/index.js
+++ packages/web3-eth-abi/src/index.js
@@ -348,13 +348,12 @@ ABICoder.prototype.decodeParameters = function (outputs, bytes) {
  */
 ABICoder.prototype.decodeParametersWith = function (outputs, bytes, loose) {
     if (outputs.length > 0 && (!bytes || bytes === '0x' || bytes === '0X')) {
-        console.error({outputs, bytes, loose})
         throw new Error(
             'Returned values aren\'t valid, did it run Out of Gas? ' +
             'You might also see this error if you are not using the ' +
             'correct ABI for the contract you are retrieving data from, ' +
             'requesting data from a block number that does not exist, ' +
-            'or querying a node which is not fully synced.'
+            'or querying a node which is not fully synced.' + ` Result: ${JSON.stringify({outputs, bytes, loose})}`
         );
     }
 
